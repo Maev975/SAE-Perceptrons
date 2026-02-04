ae36ab4699ae7116142a19887fc444c9e6cc11bb
mitsouille <melody.drouvot@gmail.com>
2026-02-04 15:37:32 +0100

    Correction GFS, une dernière prière

--- src/main/java/sae/TreeSearchAndGame/ia/algo/recherche/GFS.java
+++ src/main/java/sae/TreeSearchAndGame/ia/algo/recherche/GFS.java
@@ -7,19 +7,11 @@ import sae.TreeSearchAndGame.ia.framework.recherche.SearchProblem;
 import sae.TreeSearchAndGame.ia.framework.recherche.TreeSearch;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Map;
 import java.util.Set;
 
 public class GFS extends TreeSearch {
 
-    /**
-     * Crée un algorithme de recherche
-     *
-     * @param p Le problème à résoudre
-     * @param s L'état initial
-     */
     public GFS(SearchProblem p, State s) {
         super(p, s);
     }
@@ -28,13 +20,16 @@ public class GFS extends TreeSearch {
     public boolean solve() {
 
         ArrayList<SearchNode> frontier = new ArrayList<>();
-        ArrayList<State> visited = new ArrayList<>();
-        SearchNode root = SearchNode.makeRootSearchNode(this.initial_state);
+        Set<State> visited = new HashSet<>();
+
+        SearchNode root = SearchNode.makeRootSearchNode(initial_state);
         frontier.add(root);
 
         while (!frontier.isEmpty()) {
+
             int minIndex = 0;
             double minH = frontier.get(0).getHeuristic();
+
             for (int i = 1; i < frontier.size(); i++) {
                 double h = frontier.get(i).getHeuristic();
                 if (h < minH) {
@@ -42,24 +37,25 @@ public class GFS extends TreeSearch {
                     minIndex = i;
                 }
             }
+
             SearchNode current = frontier.remove(minIndex);
             State currentState = current.getState();
+
             if (problem.isGoalState(currentState)) {
                 this.end_node = current;
                 return true;
             }
-            if (!visited.contains(currentState)) {
-                visited.add(currentState);
-                for (Action action : problem.getActions(currentState)) {
-                    SearchNode child = SearchNode.makeChildSearchNode(problem, current, action);
-                    if (!visited.contains(child.getState())) {
-                        frontier.add(child);
-                    }
-                }
+
+            if (visited.contains(currentState)) {
+                continue;
             }
+
             visited.add(currentState);
+
             for (Action action : problem.getActions(currentState)) {
-                SearchNode child = SearchNode.makeChildSearchNode(problem, current, action);
+                SearchNode child =
+                        SearchNode.makeChildSearchNode(problem, current, action);
+
                 if (!visited.contains(child.getState())) {
                     frontier.add(child);
                 }
